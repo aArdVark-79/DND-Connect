d9c493734f2c1d15a06f9cd48a0eee90fede2238
aArdVark-79 <avrilblanksby@gmail.com>
2026-09-11 12:31:43 +1000

    Delete html.js

--- html.js
+++
@@ -1,9 +0,0 @@
-export function escapeHtml(str) {
-  const div = document.createElement('div');
-  div.textContent = str || '';
-  return div.innerHTML;
-}
-
-export function escapeAttr(str) {
-  return (str || '').replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;');
-}
